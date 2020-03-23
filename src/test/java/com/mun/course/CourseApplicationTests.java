36479e1136fa7af8dae6a69e4e78c7da36e903c5
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-03-23 20:24:23 -0300

    Project created

---
+++ src/test/java/com/mun/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mun.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
